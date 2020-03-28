2d9f9de4608825d8dfd041f63e2c084e0618c1f7
Gabor Berend <berendg@inf.u-szeged.hu>
2020-03-28 22:53:25 +0100

    removes unused code

--- js/application.js
+++ js/application.js
@@ -2,18 +2,3 @@
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
-
-
-    var imageList = [
-        "img/2.gif",
-        "img/4.gif",
-        "img/8.gif",
-        "img/16.gif",
-        "img/32.gif",
-        "img/64.gif",        
-    ];
-    for(var i = 0; i < imageList.length; i++ ) 
-    {
-        var imageObject = new Image();
-        imageObject.src = imageList[i];
-    }
